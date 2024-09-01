6f2faa915f110660336a19b972615f84ee0c0bcf
Suman Kumar <sumancsnit@gmail.com>
2024-09-01 21:26:40 +0530

    dispatch-subscribe-redux-from-component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import './index.css';
 import App from './App';
 import store from './store';
 
-store.dispatch({
-  type: 'customer/createCustomer',
-  payload: { fullName: 'Veda' },
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
