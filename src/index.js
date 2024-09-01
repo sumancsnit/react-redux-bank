e614eef277408c9a20ac1b31ab8745d588b63c76
Suman Kumar <sumancsnit@gmail.com>
2024-09-01 18:03:24 +0530

    redux state slice

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import './App.css';
 import App from './App';
-import './store';
+import store from './store';
+
+store.dispatch({ type: 'account/deposit', payload: 450 });
+
+console.log('hello redux', store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
