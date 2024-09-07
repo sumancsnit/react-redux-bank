4e77258582eb9b8c834221350fa5693fe1fe70e9
Suman Kumar <sumancsnit@gmail.com>
2024-09-07 21:47:10 +0530

    RTK createSlice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,89 +1,45 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
+  isLoading: false,
 };
 
-const accountReducer = (state = initialStateAccount, action) => {
-  switch (action.type) {
-    case 'account/deposit':
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case 'account/withdraw':
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      // LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case 'account/payLoan':
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      };
-    case 'account/convertingCurrency':
-      return {
-        ...state,
-        isLoading: true,
-      };
-    default:
-      return state;
-  }
-};
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    // replacement of action.type 'account/deposit'
+    deposit: (state, action) => {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      // prepare custom action payload value for redcer method, by default first param value is stored in action.payload in RTK
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+      reducer(state, action) {
+        // no need to return default state in RTK
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = '';
+    },
+  },
+});
 
-const deposit = (amount, currency) => {
-  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
-  // if function is returned instead of {}, redux will use thunk middleware
-  // dispatch and getState will be accessable in this fn
-  return async (dispatch, getState) => {
-    // first dispatch to show loader
-    dispatch({ type: 'account/convertingCurrency' });
-    // API Call
-    try {
-      const res = await fetch(
-        `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
-      );
-      const data = await res.json();
-      const converted = data.rates.USD;
-      // second dispatch to load actual data from api
-      dispatch({ type: 'account/deposit', payload: converted });
-    } catch (error) {
-      console.log('currency api error:', error);
-    }
+export default accountSlice.reducer;
 
-    // Return action
-  };
-};
-
-const withdraw = (amount) => {
-  return { type: 'account/withdraw', payload: amount };
-};
-
-const requestLoan = (amount, purpose) => {
-  return {
-    type: 'account/requestLoan',
-    payload: { amount, purpose },
-  };
-};
-
-const payLoan = () => {
-  return {
-    type: 'account/payLoan',
-  };
-};
-
-export default accountReducer;
-
-export { deposit, withdraw, requestLoan, payLoan };
+export const { deposit, withdraw, requestLoan, payLoan } =
+  accountSlice.actions || {};
